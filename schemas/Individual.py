1ba020244c3391317d490c5dff9c9ed89b0d33d2
roland.schier@me.com <Grassau00>
2021-05-31 20:40:04 +0200

    update on eve

--- schemas/Individual.py
+++ schemas/Individual.py
@@ -1,4 +1,4 @@
-from marshmallow import Schema, fields, validate
+from marshmallow import Schema, fields, validate, validates, ValidationError
 from schemas.medium import contMedtoTMF_Schema
 from schemas.characteristic import CharacteristicSchema
 from flask import request
@@ -8,13 +8,13 @@ class IndividualSchema(Schema):
         ordered = True
 
     id = fields.Integer(dump_only=True)
-    status = fields.String(required=True)
+    status = fields.String()
     aristocraticTitle = fields.String(required=True)
     birthDate = fields.String(required=False)
     countryOfBirth= fields.String(required=False)
     deathDate = fields.String(required=False)
-    familyName = fields.String(required=True)
-    givenName = fields.String(required=True)
+    familyName = fields.String()
+    givenName = fields.String()
     familyNamePrefix = fields.String(required=False)
     formattedName = fields.String(required=False)
     fullName=fields.String(required=False)
@@ -29,9 +29,20 @@ class IndividualSchema(Schema):
     preferredGivenName=fields.String(required=False)
     title=fields.String(required=False)
 
+    @validates('status')
+    def validate_status(self, value):
+        if value not in ['initialized', 'validated', 'deceaded']:
+            raise ValidationError('Invalid Status of PartyIndividual')
+    @validates('familyName')
+    def validate_familyName(self, value):
+        if value =='':
+            raise ValidationError('Family Name requires Value != empty')
+    @validates('givenName')
+    def validate_givenName(self, value):
+        if value =='':
+            raise ValidationError('given Name requires Value != empty')
 
-
-
+#for deserializing
 class IndivtoTMF(Schema):
     class Meta:
         ordered = True
@@ -39,26 +50,26 @@ class IndivtoTMF(Schema):
     id = fields.Integer(dump_only=True)
     href= fields.Method(serialize='buildHref')
 
-    status = fields.Str(validate=validate.OneOf(["initialized","validated","deceaded "]))
-    aristocraticTitle = fields.String(required=True)
-    birthDate = fields.String(required=False)
-    countryOfBirth = fields.String(required=False)
-    deathDate = fields.String(required=False)
-    familyName = fields.String(required=True)
-    givenName = fields.String(required=True)
-    familyNamePrefix = fields.String(required=False)
-    formattedName = fields.String(required=False)
-    fullName = fields.String(required=False)
-    gender = fields.String(required=False)
-    generation = fields.String(required=False)
-    legalName = fields.String(required=False)
-    location = fields.String(required=False)
-    maritalStatus = fields.String(required=False)
-    middleName = fields.String(required=False)
-    nationality = fields.String(required=False)
-    placeOfBirth = fields.String(required=False)
-    preferredGivenName = fields.String(required=False)
-    title = fields.String(required=False)
+    status = fields.Str()
+    aristocraticTitle = fields.String(dump_only=True)
+    birthDate = fields.String(dump_only=True)
+    countryOfBirth = fields.String(dump_only=True)
+    deathDate = fields.String(dump_only=True)
+    familyName = fields.String(dump_only=True)
+    givenName = fields.String(dump_only=True)
+    familyNamePrefix = fields.String(dump_only=True)
+    formattedName = fields.String(dump_only=True)
+    fullName = fields.String(dump_only=True)
+    gender = fields.String(dump_only=True)
+    generation = fields.String(dump_only=True)
+    legalName = fields.String(dump_only=True)
+    location = fields.String(dump_only=True)
+    maritalStatus = fields.String(dump_only=True)
+    middleName = fields.String(dump_only=True)
+    nationality = fields.String(dump_only=True)
+    placeOfBirth = fields.String(dump_only=True)
+    preferredGivenName = fields.String(dump_only=True)
+    title = fields.String(dump_only=True)
     contactMedium = fields.Nested(contMedtoTMF_Schema, attribute='contactmedium',dump_only=True,many=True)
     partyCharacteristic= fields.Nested(CharacteristicSchema, attribute='characteristic',dump_only=True,many=True)
 
--- schemas/characteristic.py
+++ schemas/characteristic.py
@@ -1,16 +1,23 @@
-from marshmallow import Schema, fields, ValidationError, validate,missing
+from marshmallow import Schema, fields, validates, ValidationError
 
 
 class CharacteristicSchema(Schema):
     class Meta:
         ordered = True
 
-    name = fields.String(required=True, error_messages={"required": "Party Character missing: Name"})
-    name = fields.String(required=True, validate=validate.Length(min=1), error_messages={"required": "Party Character Name without content"})
-
+    name = fields.String()
     valueType = fields.String(required=False)
-    value = fields.String(required=True, validate=validate.Length(min=1),error_messages={"required": "Party Character missing: Value"})
+    value = fields.String()
 
     baseType = fields.String(required=False, missing='PartyChar')
     schemaLocation = fields.String(required=False)
     type = fields.String(required=False, missing='Character')
+
+    @validates('name')
+    def validate_Name(self, value):
+        if value =='':
+            raise ValidationError("Party Character missing: Name")
+    @validates('value')
+    def validate_value(self, value):
+        if value =='':
+            raise ValidationError("Party Character missing: Value")
